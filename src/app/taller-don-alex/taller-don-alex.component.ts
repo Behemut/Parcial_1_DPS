1044489e876631a2b56108c28acbcfd6ca4706ef
Behemut <kevin_eliu@hotmail.es>
2020-08-28 12:12:35 -0600

    commit inicial

---
+++ src/app/taller-don-alex/taller-don-alex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taller-don-alex',
+  templateUrl: './taller-don-alex.component.html',
+  styleUrls: ['./taller-don-alex.component.css']
+})
+export class TallerDonAlexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
