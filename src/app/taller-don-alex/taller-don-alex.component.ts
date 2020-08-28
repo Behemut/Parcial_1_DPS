4545c4f8b84648e182a564ec5981142ecc27bf77
Behemut <kevin_eliu@hotmail.es>
2020-08-28 13:07:45 -0600

    añadiendo modulo de prueba en taller TS

--- src/app/taller-don-alex/taller-don-alex.component.ts
+++ src/app/taller-don-alex/taller-don-alex.component.ts
@@ -7,10 +7,39 @@ import {FormsModule} from '@angular/forms';
   styleUrls: ['./taller-don-alex.component.css']
 })
 export class TallerDonAlexComponent implements OnInit {
+  registros_aux=[];
+  registros=[];
+  clientes:any;
+  nombre:string;
+  dui:string;
+  vehiculo:string;
+  costo_reparacion:number;
+  visitas_contadas:number;
+  venta:number;
 
   constructor() { }
 
   ngOnInit() {
+    this.nombre="";
+    this.dui="";
+    this.costo_reparacion=0;
+    this.visitas_contadas=0;
+    this.venta=0;
   }
+
+  ingresar(){
+if (this.visitas_contadas>=2 || this.visitas_contadas<=4){
+this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.05)
+}
+if (this.visitas_contadas>4){
+  this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.10)
+}
+
+this.clientes={"nombre":this.nombre, "dui":this.dui, "costo_rep":this.costo_reparacion, "visitas":this.visitas_contadas};
+this.registros_aux.push(this.clientes);
+
+  } //Termino de metodo ingresar()
+
+
 
 }
