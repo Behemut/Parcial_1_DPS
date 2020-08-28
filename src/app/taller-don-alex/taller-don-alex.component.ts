ab5898feae06652c55ffd22a5c25290f358e90fb
Behemut <kevin_eliu@hotmail.es>
2020-08-28 15:56:59 -0600

    Primer intento de la solucion  por arrays

--- src/app/taller-don-alex/taller-don-alex.component.ts
+++ src/app/taller-don-alex/taller-don-alex.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser'
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-taller-don-alex',
@@ -16,7 +18,7 @@ export class TallerDonAlexComponent implements OnInit {
   costo_reparacion:number;
   visitas_contadas:number;
   venta:number;
-
+  contador:number;
   constructor() { }
 
   ngOnInit() {
@@ -25,21 +27,33 @@ export class TallerDonAlexComponent implements OnInit {
     this.costo_reparacion=0;
     this.visitas_contadas=0;
     this.venta=0;
+    this.contador=0;
   }
 
   ingresar(){
-if (this.visitas_contadas>=2 || this.visitas_contadas<=4){
-this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.05)
+//FORMA DE INDEXOF PARA ENCONTRAR ELEMENTO EXISTENTE EN UN ARRAY
+
+
+
+  this.clientes={"nombre":this.nombre, "dui":this.dui, "costo_rep":this.costo_reparacion, "vehiculo":this.vehiculo,"visitas":this.visitas_contadas+1};
+  this.registros.push(this.clientes);
+
+
+  console.log(this.registros.includes(this.dui));
+/*if (this.registros.includes(this.dui) ==true ){
+  if (this.visitas_contadas>=2 || this.visitas_contadas<=4){
+  this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.05)
+  }
+  if (this.visitas_contadas>4){
+    this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.10)
+  }
+  this.clientes={"nombre":this.nombre, "dui":this.dui, "costo_rep":this.costo_reparacion, "vehiculo":this.vehiculo,"visitas":this.visitas_contadas+4};
+  this.registros.push(this.clientes);
+}*/
+
+
+
+  }//Termino de metodo ingresar()
 }
-if (this.visitas_contadas>4){
-  this.venta =  this.costo_reparacion- (this.costo_reparacion* 0.10)
-}
-
-this.clientes={"nombre":this.nombre, "dui":this.dui, "costo_rep":this.costo_reparacion, "visitas":this.visitas_contadas};
-this.registros_aux.push(this.clientes);
-
-  } //Termino de metodo ingresar()
 
 
-
-}
